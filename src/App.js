e9f9a1df61285ee071073ae0ad9368a4cac994dd
WWS <fauzanfalke@gmail.com>
2020-10-19 13:13:37 +0530

    addhashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from "./components/Header/Header";
 import AllBuckets from "./pages/AllBuckets/AllBuckets";
 import Bucket from "./pages/Bucket/Bucket";
